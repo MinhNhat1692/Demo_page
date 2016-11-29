77ae3a8810bbc7a324e5c238c855f10b36074ebd
tranducck <ducck92@gmail.com>
2016-11-29 17:51:53 +0700

    Add mailer

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -18,15 +18,24 @@ class HomepageController < ApplicationController
       faraday.response :logger
       faraday.adapter :net_http
     end
+    if params[:avatar]
     img_name = params[:avatar].tempfile
     img = File.open(img_name, "r") { |io| io.read }
     result = Base64.encode64 img
     params[:avatar] = "data:#{params[:avatar].content_type};base64," + result
+    end
     res = conn.post "/api/add_customer/#{Apikey.get_admin_api}", params
     if res.status == 200
       @result = JSON.parse res.body
       if @result["message"] == "Success"
         @ordermap = @result["result"]
+        if params[:issue_place]
+          email = params[:issue_place]
+          token = params[:token]
+          number = @ordermap.id
+          name = params[:cname]
+         # CustomerMailer.notice(email, token, number, name).deliver_now
+        end
       end
     end
   end
@@ -45,7 +54,6 @@ class HomepageController < ApplicationController
       else
         @result = result
       end
-      render json: @result
     end
   end
 
